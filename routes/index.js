cae8d9ca23f8c9bb35a7688cc4f878e5e66bb13f
CaioB99 <caio_b_s@hotmail.com>
2021-09-22 20:03:11 -0300

    Criação arquivos base pag. contato

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nodes Hotel' });
 });
 
+router.get('/contato', function(req, res, next) {
+  res.render('contato');
+});
+
 module.exports = router;
