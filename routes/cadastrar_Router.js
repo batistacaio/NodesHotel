4d254edac3b40e74acd38779400871b2fb3754ae
DEV-CAB <whatever5427@gmail.com>
2022-01-05 15:26:02 -0300

    configurado o multer para upload de arquivos em pasta

--- routes/cadastrar_Router.js
+++ routes/cadastrar_Router.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var usuarioController = require("../controllers/usuarioController");
+const multer = require('multer');
+const uploadObject = multer({dest: 'destino'});
 
 
 
 router.get('/', usuarioController.showCadastro);
 
+router.post('/', uploadObject.single('foto'), function(req, res, next){
+    const file = req.file;
 
+    if(file == null){
+        const error = new Error("Arquivo da foto de perfil não foi carregado");
+        error.httpStatusCode = 400;
+        return next(error);
+
+    }
+    res.redirect('/cadastrar')
+})
 
 
 module.exports = router;
