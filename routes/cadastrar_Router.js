be1ef882e12398e3295b8e60a1a516f5b6ccd855
DEV-CAB <whatever5427@gmail.com>
2022-01-06 14:38:44 -0300

    funcionando cadastro de usuarios e uploado de fotos

--- routes/cadastrar_Router.js
+++ routes/cadastrar_Router.js
@@ -10,5 +10,20 @@ router.get('/', usuarioController.showCadastro);
 
 router.post('/', usuarioController.cadastrar);
 
+router.post('/foto', uploadObject.single('foto'), function(req, res, next){
+    const file = req.file;
+
+    if(file == null){
+        const error = new Error("Arquivo vazio");
+        error.httpStatusCode = 400;
+        return next(error);
+    }else{
+     
+     res.redirect('/cadastrar');
+     function alerta(){alert("arquivo carregado com sucesso!")}
+    }
+
+    });
+
 
 module.exports = router;
