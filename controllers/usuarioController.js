81dedeccf7dc5d38e5bc71fa9427de24e30efc94
DEV-CAB <whatever5427@gmail.com>
2022-01-06 11:01:37 -0300

    finalizando as configurações de cadastro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,5 @@
 const usuarios = [];
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
 const usuarioController = {
     showCadastro: function(req,res,next){
@@ -9,16 +9,25 @@ const usuarioController = {
       res.render('login_usuario')
     },
     cadastrar: function(req, res, next){
-      const { nome, sobrenome } = req.body;
+      const { country, nome, sobrenome, email, cpf, ddd, number, nascimento, endereco, cep, bairro, cidade, uf, usuario, senha  } = req.body;
       usuarios.push(
         {
+        country: country,
         nome: nome,
         sobrenome: sobrenome,
+        email: email,
+        cpf: cpf,
+        ddd: ddd,
+        telefone: number,
+        data_de_nascimento: nascimento,
+        endereço: [endereco + ' ,'+ "bairro :" +bairro +" "+ "cep:" + cep +" " + cidade +" "+ uf],
+        usuario: usuario,
+        senha: senha
 
         });
         res.send("usuario cadastrado com sucesso")
         console.log(usuarios)
     }
-  }
+  };
   
   module.exports = usuarioController;
