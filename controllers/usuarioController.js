7e31c8fa9b7cb68b77b46e7077535c261e02f315
DEV-CAB <whatever5427@gmail.com>
2022-01-05 21:32:06 -0300

    teste

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,23 @@
+const usuarios = [];
+const bcrypt = require('bcryptjs')
+
 const usuarioController = {
     showCadastro: function(req,res,next){
       res.render('cadastrar_usuario')
     },
     showLogin: function(req, res, next){
       res.render('login_usuario')
+    },
+    cadastrar: function(req, res, next){
+      const { nome, sobrenome } = req.body;
+      usuarios.push(
+        {
+        nome: nome,
+        sobrenome: sobrenome,
+
+        });
+        res.send("usuario cadastrado com sucesso")
+        console.log(usuarios)
     }
   }
   
--- routes/cadastrar_Router.js
+++ routes/cadastrar_Router.js
@@ -8,17 +8,7 @@ const uploadObject = multer({dest: 'destino'});
 
 router.get('/', usuarioController.showCadastro);
 
-router.post('/', uploadObject.single('foto'), function(req, res, next){
-    const file = req.file;
-
-    if(file == null){
-        const error = new Error("Arquivo da foto de perfil não foi carregado");
-        error.httpStatusCode = 400;
-        return next(error);
-
-    }
-    res.redirect('/cadastrar')
-})
+router.post('/', usuarioController.cadastrar);
 
 
 module.exports = router;
