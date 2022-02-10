b47fbd7a61940822d1e4c746cb46b0cb831c758b
DEV-CAB <whatever5427@gmail.com>
2022-02-10 11:13:47 -0300

    criadas funções js para habilitar o botao cadastrar

--- public/javascript-front.js
+++ public/javascript-front.js
@@ -1,14 +1,38 @@
-function pass_message(){
-    document.getElementById("senha")
-    alert("insira a senha com no minímo 6 caracteres e com pelo menos 1 número");
-}
+// function pass_message(){
+//     document.getElementById("senha")
+//     alert("insira a senha com no minímo 6 caracteres e com pelo menos 1 número");
+// };
 
 
-function check_passwords(){
+// function check_passwords(){
+//     var senha = document.getElementById('senha')
+//     var confirmacao = document.getElementById('confirmacao')
+
+// if(senha.value != confirmacao.value){
+//     alert("as senhas não conferem")
+// }
+// };
+
+function habilitar(){
     var senha = document.getElementById('senha')
-    var confirme = documento.getElementById('confirmacao')
+    var confirmacao = document.getElementById('confirmacao')
+    var container = document.querySelector('.msg')
+    if(senha.value == confirmacao.value && senha.value != "" && confirmacao.value != ""){
+    document.getElementById('botao').disabled = false;
+    container.style.display = 'none'
+   
+}};
+function desabilitar(){
+    var senha = document.getElementById('senha')
+    var confirmacao = document.getElementById('confirmacao')
+    var container = document.querySelector('.msg') 
+    if(senha.value != confirmacao.value){
+    document.getElementById('botao').disabled = true
+    container.style.display = 'block'
+   
+}};
 
-if(senha.value != confirme.value){
-    alert("as senhas não conferem")
-}
-}
+
+
+
+
