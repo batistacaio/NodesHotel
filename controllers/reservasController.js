7c658b14c55df1754eccfd2e100333085fae8ff1
DEV-CAB <whatever5427@gmail.com>
2022-02-13 10:02:56 -0300

    ajuste

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -1,13 +1,18 @@
 const reservasController = {
-    index: (req,res,next) => {
-      if(!("usuario_logado" in req.session) || req.session.usuario_logado == false){
-        res.send("você precisa se logar no site para ter acesso à página de reservas")
-      }else{
+//     index: (req,res,next) => {
+//       if(!("usuario_logado" in req.session) || req.session.usuario_logado == false){
+//         res.send("você precisa se logar no site para ter acesso à página de reservas")
+//       }else{
     
-      res.render('reservas')
-    }
-  }
-};
+//       res.render('reservas')
+//     }
+//   }
+// };
+// este codigo foi temporariamente comentado para permitir o acesso a view de reservas pois o projeto apresenta problema na session.
+index: (req, res, next)=>{
+  res.render('reservas')
+}
+}
 
   module.exports = reservasController;
 
