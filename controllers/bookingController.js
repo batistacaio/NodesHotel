d2c4bce7a1b1cac1386b533ae847ebe80c20a30d
DEV-CAB <whatever5427@gmail.com>
2022-02-13 14:56:32 -0300

    teste

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,3 +1,6 @@
+// const { parseTwoDigitYear } = require('moment');
+// const moment = require('moment');
+
 const reserva = [];
 
 const bookingController = {
@@ -8,6 +11,40 @@ const bookingController = {
 
   reservar: function(req, res, next){
     const { entrada, saida, hospedes, suites, hospede1, hospede2, hospede3, hospede4  } = req.body;
+    const diarias = [
+
+  {suite :"PLAZA", diaria: 240.00},
+  {suite: "ROMA", diaria: 280.00},
+  {suite: "OMEGA", diaria: 200.00},
+  {suite: "GARDEN", diaria: 180.00},
+  {suite: "MASTER", diaria: 220.00},
+  {suite: "GOLD", diaria: 340.00},
+  {suite: "ROYAL", diaria: 320.00},
+  {suite: "SILVER", diaria: 250.00},
+  
+  ];
+
+
+var chegada = new Date(entrada);
+var partida = new Date(saida);
+
+var timeDiff = Math.abs(chegada.getTime() - partida.getTime());
+var diffDays = Math.ceil(timeDiff / (1000 *3600*24))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       //   a minha função de calculo de quant de diarias deve ser feita aqui
       //  e o a multiplicaçao do total de dias e dos valor da suite aqui.
@@ -21,11 +58,13 @@ const bookingController = {
           hospede1: hospede1,
           hospede2: hospede2,
           hospede3: hospede3,
-          hospede4: hospede4
+          hospede4: hospede4,
+          total_dias: 40,
+          valor_reserva: 1400.00
         });
 
      res.render('resumo')
-     console.log(reserva)
+     console.log(diffDays)
 
         }
       };
